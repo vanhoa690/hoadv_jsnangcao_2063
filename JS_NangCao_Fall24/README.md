483e79e4d91717f4ddd4696e109784fc923cc6e2
vanhoa690 <vanhoa690@gmail.com>
2024-09-25 12:55:32 +0700

    done call api

--- JS_NangCao_Fall24/README.md
+++ JS_NangCao_Fall24/README.md
@@ -16,3 +16,79 @@ console.log("Xin chao " + name); // echo php....
 }
 
 sayHello("Hoadv");
+
+## Install Json server (BE - Call API): http://localhost:3000/products
+
+npm i json-server
+package-json
+"scripts": {
+"server": "json-server db.json"
+},
+npm run sever
+http://localhost:3000/products
+
+## Day 2: Render Product List
+
+// 1. Truy cap DOM: document.getElementById(id)
+const listElement = document.getElementById("list");
+console.log(listElement);
+
+// 2. innerHTML: update DOM, su dung ``: Huyen doi (ben trai so 1)
+
+// 3: Chen Du Lieu
+const products = [
+{
+id: 1,
+title: "San pham AA+",
+price: 100000,
+},
+{
+id: 2,
+title: "San pham B+",
+price: 100000,
+},
+{
+id: 2,
+title: "San pham C+",
+price: 100000,
+},
+];
+
+function sayHello() {}
+// arrow function
+const sayHello2 = () => {};
+
+// map: [1,2] => function (item x 2) => [2, 4]
+//products: [{}] => [`<tr></tr>`, `<tr></tr>`]
+
+function renderRowTable(product) {
+return `       <tr>
+            <th scope="row">${product.id}</th>
+            <td>${product.title}</td>
+            <td>${product.price} VND</td>
+            <td>
+              <button class="btn btn-danger">Delete</button>
+              <button class="btn btn-info">Edit</button>
+            </td>
+          </tr>`;
+}
+
+// const test = products.map(renderRowTable).join("");
+// console.log(test);
+
+listElement.innerHTML = `
+
+ <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        ${products.map(renderRowTable).join("")}
+        </tbody>
+      </table>
+      `;
--- JS_NangCao_Fall24/script.js
+++ JS_NangCao_Fall24/script.js
@@ -1,39 +1,33 @@
-// 1. Truy cap DOM: document.getElementById(id)
 const listElement = document.getElementById("list");
-console.log(listElement);
 
-// 2. innerHTML: update DOM, su dung ``: Huyen doi (ben trai so 1)
+// Destructuring_assignment
+// {data} = data
+async function getProductList() {
+  const { data } = await axios.get("http://localhost:3000/products");
 
-// 3: Chen Du Lieu
-const products = [
-  {
-    id: 1,
-    title: "San pham AA+",
-    price: 100000,
-  },
-  {
-    id: 2,
-    title: "San pham B+",
-    price: 100000,
-  },
-  {
-    id: 2,
-    title: "San pham C+",
-    price: 100000,
-  },
-];
+  listElement.innerHTML = `
+  <table class="table">
+         <thead>
+           <tr>
+             <th scope="col">Id</th>
+             <th scope="col">Title</th>
+             <th scope="col">Price</th>
+             <th scope="col">Actions</th>
+           </tr>
+         </thead>
+         <tbody>
+         ${data.map(renderRowTable).join("")}
+         </tbody>
+       </table>
+       `;
+}
 
-function sayHello() {}
-// arrow function
-const sayHello2 = () => {};
-
-// map: [1,2] => function (item x 2) => [2, 4]
-//products: [{}] => [`<tr></tr>`, `<tr></tr>`]
+getProductList();
 
 function renderRowTable(product) {
   return `
         <tr>
-            <th scope="row">1</th>
+            <th scope="row">${product.id}</th>
             <td>${product.title}</td>
             <td>${product.price} VND</td>
             <td>
@@ -43,22 +37,3 @@ function renderRowTable(product) {
           </tr>
  `;
 }
-
-// const test = products.map(renderRowTable).join("");
-// console.log(test);
-
-listElement.innerHTML = `
- <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Title</th>
-            <th scope="col">Price</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-        ${products.map(renderRowTable).join("")}
-        </tbody>
-      </table>
-      `;
