9fa7e8bb22d56d607c0dc8d9b09020dec0d39e5d
vanhoa690 <vanhoa690@gmail.com>
2024-10-02 12:46:21 +0700

    product add

--- JS_NangCao_Fall24/add.js
+++ JS_NangCao_Fall24/add.js
@@ -1,1 +1,19 @@
-console.log("add");
+async function handleSubmit(event) {
+  // 1.chặn hành động lan truyền lên phía trên
+  event.preventDefault();
+
+  // 2. Lay data tu input nhap vao
+  const titleElement = document.getElementById("title");
+  const priceElement = document.getElementById("price");
+
+  // 3. Call API add product POST
+  const newProduct = {
+    title: titleElement.value,
+    price: Number(priceElement.value),
+  };
+
+  console.log(newProduct);
+
+  await axios.post("http://localhost:3000/products", newProduct);
+  location.href = "/";
+}
