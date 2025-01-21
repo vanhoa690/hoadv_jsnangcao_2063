4979ad45afd8a38d8c5d331aeca75320272172bf
vanhoa690 <vanhoa690@gmail.com>
2025-01-21 19:50:13 +0700

    done day 3

--- jsnc_sp25/index.js
+++ jsnc_sp25/index.js
@@ -3,17 +3,33 @@
 // 2. Template Literals ``
 // 3. map, callback trong map
 // 4. arrow function () => {}
+// 5 nodejs
+//6 json server
+// 7 db.json
+// 8 chay server: json-server --watch db.json
+// 9 Kiểm tra API: REST API
+// 10. gọi API trong JavaScript:
+// 4 cach: 1.fetch().then() 2.fetch() async/await 3.axios().then() 4.axios() async/await
+// 10.1: fetch()
+// 10.2 : axios
+// 11. Xu ly bat dong bo trong javascript: async/await or then
 
-const products = [
-  { id: 1, name: "Sản phẩm A", price: 100 },
-  { id: 2, name: "Sản phẩm B", price: 200 },
-  { id: 3, name: "Sản phẩm C", price: 300 },
-  { id: 4, name: "Sản phẩm D", price: 400 },
-  { id: 5, name: "Sản phẩm E", price: 500 },
-  { id: 6, name: "Sản phẩm F", price: 600 },
-  { id: 7, name: "Sản phẩm H", price: 700 },
-  { id: 8, name: "Sản phẩm H", price: 700 },
-];
+async function getProducts() {
+  try {
+    const response = await axios.get("http://localhost:3000/products");
+    console.log("Products:", response.data); // Hiển thị danh sách sản phẩm
+
+    const products = response.data;
+    // Tạo HTML từ mảng sản phẩm
+    const htmlContent = products.map(renderRow).join("");
+
+    document.getElementById("list").innerHTML = htmlContent;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
+
+getProducts();
 
 // callback(currentValue) dung cho map
 function renderRow(product) {
@@ -22,6 +38,7 @@ function renderRow(product) {
       <td>${product.id}</td>
       <td>${product.name}</td>
       <td>Giá: ${product.price} VNĐ</td>
+      <td>${product.category}</td>
       <td>
         <button class="btn btn-danger">Delete</button>
         <button class="btn btn-info">Edit</button>
@@ -30,7 +47,4 @@ function renderRow(product) {
   `;
 }
 
-// Tạo HTML từ mảng sản phẩm
-const htmlContent = products.map(renderRow).join("");
-
-document.getElementById("list").innerHTML = htmlContent;
+function deleProduct() {}
