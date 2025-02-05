fe5289b0206df84329ce61cfa947831b51497453
vanhoa690 <vanhoa690@gmail.com>
2025-02-05 08:51:34 +0700

    call api get product list

--- jsnc_sp25/index.js
+++ jsnc_sp25/index.js
@@ -1,6 +1,6 @@
 // show html = javascript
 // 1. innerHTML
-// 2. Template Literals ``
+// 2. Template Literals `` (canh so 1) ko phai ' ""
 // 3. map, callback trong map
 // 4. arrow function () => {}
 // 5 nodejs
@@ -9,21 +9,14 @@
 // 8 chay server: json-server --watch db.json
 // 9 Kiểm tra API: REST API
 // 10. gọi API trong JavaScript:
-// 4 cach: 1.fetch().then() 2.fetch() async/await 3.axios().then() 4.axios() async/await
+// 4 cach:
+// 1.fetch().then(callback function)
+// 2.fetch() async/await
+// 3.axios().then()
+// 4.axios() async/await (chon cach nay)
 // 10.1: fetch()
 // 10.2 : axios
 // 11. Xu ly bat dong bo trong javascript: async/await or then
-
-const products = [
-  { id: 1, name: "Sản phẩm A", price: 100 },
-  { id: 2, name: "Sản phẩm B", price: 200 },
-  { id: 3, name: "Sản phẩm C", price: 300 },
-  { id: 4, name: "Sản phẩm D", price: 400 },
-  { id: 5, name: "Sản phẩm E", price: 500 },
-  { id: 6, name: "Sản phẩm F", price: 600 },
-  { id: 7, name: "Sản phẩm H", price: 700 },
-  { id: 8, name: "Sản phẩm H", price: 700 },
-];
 
 // callback(currentValue) dung cho map
 function renderRow(product) {
@@ -32,6 +25,7 @@ function renderRow(product) {
       <td>${product.id}</td>
       <td>${product.name}</td>
       <td>Giá: ${product.price} VNĐ</td>
+       <td>${product.category}</td>
       <td>
         <button class="btn btn-danger">Delete</button>
         <button class="btn btn-info">Edit</button>
@@ -40,7 +34,25 @@ function renderRow(product) {
   `;
 }
 
-// Tạo HTML từ mảng sản phẩm
-const htmlContent = products.map(renderRow).join("");
+// Call API Lay danh sach san pham
+async function getProducts() {
+  try {
+    const response = await axios.get("http://localhost:3000/products");
+    console.log("Products:", response.data); // response: config, header, data, status
 
-document.getElementById("list").innerHTML = htmlContent;
+    const products = response.data;
+
+    // Tạo HTML từ mảng sản phẩm
+    const htmlContent = products.map(renderRow).join("");
+
+    document.getElementById("list").innerHTML = htmlContent;
+  } catch (error) {
+    console.error("Show ra loi Error fetching products:", error);
+  }
+}
+
+// chay ham hien thi danh sach san pham
+getProducts();
+
+// xoa san pham
+async function deleteProduct() {}
