4533d6ce30008ed7f2420fd64da297a229951879
vanhoa690 <vanhoa690@gmail.com>
2025-02-12 08:45:30 +0700

    done add product

--- jsnc_sp25/add.js
+++ jsnc_sp25/add.js
@@ -1,1 +1,29 @@
-console.log("aaa");
+async function handleSubmit(event) {
+  event.preventDefault(); // Ngăn chặn reload trang
+
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const category = document.getElementById("category").value;
+
+  //   Kiem tra du lieu: Validate
+  if (!name || !price || price < 0) {
+    return alert("Vui lòng nhập tên sản phẩm va gia hợp lệ!");
+  }
+
+  // Them du lieu vao db.json
+  try {
+    const data = {
+      name,
+      price: Number(price),
+      category,
+    };
+    const product = await axios.post("http://localhost:3000/products", data);
+
+    location.href = "/";
+
+    alert("them thanh cong");
+  } catch (error) {
+    console.log(error.message);
+    alert(error.message);
+  }
+}
